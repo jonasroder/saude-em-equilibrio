87615524de57ac058dc32e5b9d195703dab29f28
Jonas Roder <jonasroder@gmail.com>
2023-12-08 21:55:39 -0300

    Troca do gradle para mavin

--- src/main/resources/db/migration/V9__create-table-usuario.sql
+++ src/main/resources/db/migration/V9__create-table-usuario.sql
@@ -8,11 +8,3 @@ CREATE TABLE usuario
     id_situacao INT,
     is_active   TINYINT(1) DEFAULT 1
 );
-
-
-
-INSERT INTO usuario (id_pessoa, usuario, senha, id_situacao, is_active)
-VALUES (1, 'jonasroder', '$2a$10$oBP684358Tg35ISnn9oP2O6N4mWjRhccWQ8IX9KlgX2cfLdThxO86', 1, 1);
-
-INSERT INTO usuario (id_pessoa, usuario, senha, id_situacao, is_active)
-VALUES (2, 'deniseroder', '$2a$10$dcq99u2wxKJ4T13HU79ZmuVzuVqzNHru3ivWppawXYH0jPY7YR1oG', 1, 1);
